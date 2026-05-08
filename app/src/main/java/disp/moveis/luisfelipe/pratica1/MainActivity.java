70eddd97daac2a4dd0956bc0c94faf73d33f82fb
Luis Felipe Martins Pereira <luis.f.pereira@ufv.br>
2026-05-08 15:37:18 -0300

    inicio da atividade3

--- app/src/main/java/disp/moveis/luisfelipe/pratica1/MainActivity.java
+++ app/src/main/java/disp/moveis/luisfelipe/pratica1/MainActivity.java
@@ -1,8 +1,14 @@
 package disp.moveis.luisfelipe.pratica1;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,18 +18,47 @@ import androidx.core.view.WindowInsetsCompat;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
 
+        String menu[] = new String[]{
+            "Minha casa na cidade natal",
+            "Minha casa em Viçosa",
+            "Meu departamento",
+            "Sair da aplicação"
+        };
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+    this,
+            android.R.layout.simple_list_item_1,
+            menu
+            );
+
+        setListAdapter(arrayAdapter);
+        };
+
+    public void onListItemClick(ListView l, View v, int position, long id){
+        Intent it = new Intent(this, MapActivity.class);
+        String aux = l.getItemAtPosition(position).toString();
+
+        switch(position){
+            case 0:
+                Toast.makeText(getBaseContext(), aux, Toast.LENGTH_SHORT).show();
+                startActivity(it);
+                break;
+            case 1:
+                Toast.makeText(getBaseContext(), aux, Toast.LENGTH_SHORT).show();
+                startActivity(it);
+                break;
+            case 2:
+                Toast.makeText(getBaseContext(), aux, Toast.LENGTH_SHORT).show();
+                startActivity(it);
+                break;
+            case 3:
+                finish();
+        }
+    }
 }
