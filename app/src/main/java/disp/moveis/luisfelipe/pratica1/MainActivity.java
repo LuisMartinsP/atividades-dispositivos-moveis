56c9d52ef3afc7ea7fe2d95e2bbcd91b6f785ed4
Luis Felipe Martins Pereira <luis.f.pereira@ufv.br>
2026-05-30 23:11:14 -0300

    Correção da atividade 4 A

--- app/src/main/java/disp/moveis/luisfelipe/pratica1/MainActivity.java
+++ app/src/main/java/disp/moveis/luisfelipe/pratica1/MainActivity.java
@@ -1,5 +1,6 @@
 package disp.moveis.luisfelipe.pratica1;
 
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorAdditionalInfo;
 import android.hardware.SensorEvent;
@@ -11,6 +12,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,39 +23,83 @@ import com.google.android.material.materialswitch.MaterialSwitch;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity
+        implements SensorEventListener {
+
     private SensorManager sensorManager;
     private Sensor sensorL;
     private Sensor sensorP;
+
+    private float ultimoLux;
+    private float ultimaProximidade;
+
     private MaterialSwitch switchLanterna;
     private MaterialSwitch switchViber;
-    private MotorHelper motor;
-    private LanternaHelper lanterna;
 
-    private float ultimoLux = 0;
-    private float ultimaProximidade = 0;
+    private LanternaHelper lanterna;
+    private MotorHelper motor;
+
+    private ActivityResultLauncher<Intent> launcher;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-        sensorL = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        sensorP = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
         switchLanterna = findViewById(R.id.lanternaSwitcher);
         switchViber = findViewById(R.id.viberSwitcher);
+
+        sensorManager =
+                (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensorL =
+                sensorManager.getDefaultSensor(
+                        Sensor.TYPE_LIGHT);
+
+        sensorP =
+                sensorManager.getDefaultSensor(
+                        Sensor.TYPE_PROXIMITY);
+
         lanterna = new LanternaHelper(this);
         motor = new MotorHelper(this);
+
+        launcher =
+                registerForActivityResult(
+                        new ActivityResultContracts.StartActivityForResult(),
+                        result -> {
+
+                            if(result.getResultCode() == RESULT_OK &&
+                                    result.getData() != null){
+
+                                Intent data = result.getData();
+
+                                boolean ligarLanterna =
+                                        data.getBooleanExtra(
+                                                "ligarLanterna",
+                                                false);
+
+                                boolean ligarVibracao =
+                                        data.getBooleanExtra(
+                                                "ligarVibracao",
+                                                false);
+
+                                if(ligarLanterna){
+                                    lanterna.ligar();
+                                    switchLanterna.setChecked(true);
+                                }else{
+                                    lanterna.desligar();
+                                    switchLanterna.setChecked(false);
+                                }
+
+                                if(ligarVibracao){
+                                    motor.iniciarVibracao();
+                                    switchViber.setChecked(true);
+                                }else{
+                                    motor.pararVibracao();
+                                    switchViber.setChecked(false);
+                                }
+                            }
+                        });
     }
 
     @Override
@@ -62,15 +109,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager.registerListener(
                 this,
                 sensorL,
-                SensorManager.SENSOR_DELAY_NORMAL
-        );
+                SensorManager.SENSOR_DELAY_NORMAL);
 
         sensorManager.registerListener(
                 this,
                 sensorP,
-                SensorManager.SENSOR_DELAY_NORMAL
-        );
+                SensorManager.SENSOR_DELAY_NORMAL);
     }
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -78,50 +124,48 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     @Override
-    protected void onStop(){
-        super.onStop();
-        lanterna.desligar();
-        motor.pararVibracao();
-    }
-    @Override
     protected void onDestroy() {
         lanterna.desligar();
         motor.pararVibracao();
         super.onDestroy();
     }
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
 
-        if (sensorEvent.sensor.getType() == Sensor.TYPE_LIGHT) {
-            ultimoLux = sensorEvent.values[0];
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if(event.sensor.getType()
+                == Sensor.TYPE_LIGHT){
+
+            ultimoLux = event.values[0];
         }
 
-        if (sensorEvent.sensor.getType() == Sensor.TYPE_PROXIMITY) {
-            ultimaProximidade = sensorEvent.values[0];
+        if(event.sensor.getType()
+                == Sensor.TYPE_PROXIMITY){
+
+            ultimaProximidade = event.values[0];
         }
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
+    public void onAccuracyChanged(
+            Sensor sensor,
+            int accuracy) {
     }
 
     public void classificar_leituras(View v){
 
-        if (ultimoLux < 5) {
-            switchLanterna.setChecked(true);
-            lanterna.ligar();
-        } else {
-            switchLanterna.setChecked(false);
-            lanterna.desligar();
-        }
+        Intent intent =
+                new Intent(
+                        "disp.moveis.luisfelipe.CLASSIFICAR_LEITURAS");
 
-        if (ultimaProximidade < (sensorP.getMaximumRange())) {
-            switchViber.setChecked(true);
-            motor.iniciarVibracao();
-        } else {
-            switchViber.setChecked(false);
-            motor.pararVibracao();
-        }
+        intent.putExtra(
+                "lux",
+                ultimoLux);
+
+        intent.putExtra(
+                "proximidade",
+                ultimaProximidade);
+
+        launcher.launch(intent);
     }
 }
